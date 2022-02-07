301e6508190ac8a262344479afb76a923900b402
LonnonjamesD <lonnonjamesd@protonmail.com>
2022-02-06 21:16:45 -0500

    Fixed Physical Device

--- src/vulkan/safe_wrapper/physical_device.rs
+++ src/vulkan/safe_wrapper/physical_device.rs
@@ -1,33 +1,24 @@
-use std::{ptr, sync::Arc};
+use std::{sync::Arc};
 
 use mira::vulkan::{VkPhysicalDevice};
 
 
+use crate::{vulkan::safe_wrapper::functions::get_physical_devices, match_error_codes::MatchErrorCode};
+
 use super::instance::Instance;
 
 pub struct PhysicalDevice {
-    physical_device: VkPhysicalDevice,
-    device_count: u32,
+    physical_device: Vec<VkPhysicalDevice>,
 }
 
 impl PhysicalDevice {
     pub fn new(instance: Arc<Instance>) -> Self {
-        println!("code ran?");
-        let new_instance = instance.clone();
-        println!("code ran?");
-        let device_count = 0 as *mut u32;
-        let mut devices: VkPhysicalDevice = unsafe { std::mem::zeroed() };
-        println!("code ran?");
-        let device_result = vkEnumeratePhysicalDevices(new_instance.instance, device_count, &mut devices as *mut VkPhysicalDevice);
-        println!("code ran?");
-        if device_count as i32 == 0 {
-            panic!("No physical devices found");
+        let devices = get_physical_devices((*instance.clone()).instance);
+        match devices {
+            Ok(devices) => PhysicalDevice {
+                physical_device: devices,
+            },
+            Err(error) => panic!("Failed to get physical devices: {}", error.match_error_code()),
         }
-        
-        println!("{:?}", devices);
-        unsafe { PhysicalDevice { 
-            physical_device: ptr::null_mut(),
-            device_count:  *device_count,
-        }}
     }
 }
