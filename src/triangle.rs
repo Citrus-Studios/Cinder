01534569ec1df3a0c3749d864ff7e68b962babab
LonnonjamesD <lemonjameslem@protonmail.com>
2022-02-09 20:59:39 -0500

    Added Engine Name

--- src/triangle.rs
+++ src/triangle.rs
@@ -3,7 +3,8 @@ fn triangle() {
     use crate::vulkan::safe_wrapper::{instance::InstanceBuilder, physical_device::PhysicalDevice};
 
     let instance = InstanceBuilder::new()
-        .application_name("Triangle")
+        .application_name("Cinder")
+        .engine_name("None")
         .build();
 
     let _physical_device = PhysicalDevice::new(instance).pick_best_device();
