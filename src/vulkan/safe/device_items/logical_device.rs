3741e8266c50944838e201bcd36d68c8c6666046
LonnonjamesD <lemonjameslem@protonmail.com>
2022-02-19 20:52:33 -0500

    Implemented new and replaced into_raw

--- src/vulkan/safe/device_items/logical_device.rs
+++ src/vulkan/safe/device_items/logical_device.rs
@@ -1,11 +1,27 @@
-use mira::vulkan::VkDevice;
+use mira::vulkan::{VkDevice, VkAllocationCallbacks};
+
+use crate::vulkan::safe::{functions::create_device, };
+
+use super::{physical_device::PhysicalDevice, device_create_info::DeviceCreateInfo};
+
+#[test]
+fn create_device_test() {
+    use crate::vulkan::safe::{instance_items::instance::InstanceBuilder, device_items::device_create_info::DeviceCreateInfoBuilder};
+
+    let instance = InstanceBuilder::new().build();
+    let physical_device = PhysicalDevice::new(instance);
+    let device_create_info = DeviceCreateInfoBuilder::<()>::new().build();
+    let device = LogicalDevice::new(physical_device, Some(device_create_info), None);
+}
 
 pub struct LogicalDevice {
     device: VkDevice,
 }
 
 impl LogicalDevice {
-    pub fn into_raw(self) -> VkDevice {
-        todo!();
+    pub fn new<T: Clone>(physical_device: PhysicalDevice, create_info: Option<DeviceCreateInfo<T>>, allocator: Option<VkAllocationCallbacks>) -> Self {
+        LogicalDevice {
+            device: create_device(physical_device, create_info, allocator).unwrap(),
+        }
     }
 }
