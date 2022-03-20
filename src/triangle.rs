a2e9d74fd25b3be3ad8026251164f96f3a6fd784
LonnonjamesD <lemonjameslem@protonmail.com>
2022-03-20 00:55:10 -0400

    extensions with SDL (winit why do you make it hard)

--- src/triangle.rs
+++ src/triangle.rs
@@ -1,6 +1,8 @@
+
 #[test]
 fn triangle() {
     use crate::vulkan::safe::{instance_items::instance::InstanceBuilder, device_items::physical_device::PhysicalDevice};
+    use sdl2::sys::SDL_Vulkan_GetInstanceExtensions;
 
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
@@ -11,11 +13,22 @@ fn triangle() {
         .build()
         .unwrap();
 
+    let mut count:u32 = 0;
+    unsafe { SDL_Vulkan_GetInstanceExtensions(window.raw(), &mut count, std::ptr::null_mut()) };
+
+    let mut extensions = unsafe { mira::mem::zeroed_vec(count as usize) };
+    unsafe { SDL_Vulkan_GetInstanceExtensions(window.raw(), &mut count, extensions.as_mut_ptr()) };
+
+    let extensions = extensions.into_iter().map(|x| {
+        unsafe { std::ffi::CStr::from_ptr(x).to_str().unwrap() }
+    }).collect();
+
     let instance = InstanceBuilder::new()
         .application_name("Cinder")
+        .extensions(vec![vec!["VK_KHR_xlib_surface"], extensions].concat())
         .engine_name("None")
         .build();
 
-    let _physical_device = PhysicalDevice::new(instance, &window).pick_best_device();
+    // let _physical_device = PhysicalDevice::new(instance, &window).pick_best_device();
 }
 
