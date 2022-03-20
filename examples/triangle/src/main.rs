77e391b5e4da3801e2f6d454d135cffd91d5c62a
LonnonjamesD <lemonjameslem@protonmail.com>
2022-03-20 18:28:15 -0400

    Triangle Example

--- examples/triangle/src/main.rs
+++ examples/triangle/src/main.rs
@@ -1,9 +1,8 @@
+use cinder::vulkan::safe::{instance_items::instance::InstanceBuilder, device_items::physical_device::PhysicalDevice};
+use sdl2::sys::SDL_Vulkan_GetInstanceExtensions;
+use cinder::zeroed_vec;
 
-#[test]
-fn triangle() {
-    use crate::vulkan::safe::{instance_items::instance::InstanceBuilder, device_items::physical_device::PhysicalDevice};
-    use sdl2::sys::SDL_Vulkan_GetInstanceExtensions;
-
+fn main() {
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
 
@@ -16,19 +15,22 @@ fn triangle() {
     let mut count:u32 = 0;
     unsafe { SDL_Vulkan_GetInstanceExtensions(window.raw(), &mut count, std::ptr::null_mut()) };
 
-    let mut extensions = unsafe { mira::mem::zeroed_vec(count as usize) };
+    let mut extensions = unsafe { zeroed_vec(count as usize) };
     unsafe { SDL_Vulkan_GetInstanceExtensions(window.raw(), &mut count, extensions.as_mut_ptr()) };
+
+    
 
     let extensions = extensions.into_iter().map(|x| {
         unsafe { std::ffi::CStr::from_ptr(x).to_str().unwrap() }
     }).collect();
+    println!("{:?}", extensions);
 
     let instance = InstanceBuilder::new()
         .application_name("Cinder")
         .extensions(extensions)
+        .layers(vec!["VK_LAYER_KHRONOS_validation"])
         .engine_name("None")
         .build();
 
     // let _physical_device = PhysicalDevice::new(instance, &window).pick_best_device();
 }
-
