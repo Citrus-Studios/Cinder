05ea687b863f2ee2238af09b5029ae5cef12f40b
LonnonjamesD <lemonjameslem@protonmail.com>
2022-03-21 19:48:41 -0400

    Merged Local Branch

--- src/vulkan/safe/instance_items/instance.rs
+++ src/vulkan/safe/instance_items/instance.rs
@@ -1,42 +1,45 @@
-use std::{sync::Arc};
+use std::sync::Arc;
 
 use mira::{vulkan::{self as vk, VkInstanceCreateInfo}};
 #[allow(unused_imports)]
 use const_cstr::*;
 use vk::{VkInstance, VK_MAKE_API_VERSION};
 
-
 use crate::match_error_codes::MatchErrorCode;
 
-use super::{application_info::ApplicationInfoBuilder, instance_create_info::InstanceCreateInfoBuilder, super::functions::create_instance};
+use super::{
+    super::functions::create_instance, application_info::ApplicationInfoBuilder,
+    instance_create_info::{InstanceCreateInfoBuilder, InstanceCreateInfo},
+};
 
-#[derive(Clone, Copy)]
 pub struct Instance {
     pub(crate) instance: VkInstance,
     create_info: VkInstanceCreateInfo,
 }
 
 pub struct InstanceBuilder<'a> {
-    application_name: Option<&'a str>, 
-    engine_name: Option<&'a str>, 
-    vulkan_version: Option<u32>, 
+    application_name: Option<&'a str>,
+    engine_name: Option<&'a str>,
+    vulkan_version: Option<u32>,
     application_version: Option<u32>,
     engine_version: Option<u32>,
     extensions: Option<Vec<String>>,
+    layers: Option<Vec<String>>,
 }
 
 impl Instance {
     pub fn new(
-        application_name: Option<&str>, 
-        engine_name: Option<&str>, 
-        vulkan_version: Option<u32>, 
+        application_name: Option<&str>,
+        engine_name: Option<&str>,
+        vulkan_version: Option<u32>,
         application_version: Option<u32>,
         engine_version: Option<u32>,
-        extensions: Option<Vec<String>>
+        extensions: Option<Vec<String>>,
+        layers: Option<Vec<String>>,
     ) -> Arc<Self> {
         // Make the application info
-        let application_name = application_name.unwrap_or("Cinder");
-        let engine_name = engine_name.unwrap_or("None");
+        let application_name = application_name.unwrap_or("Cinder").to_string();
+        let engine_name = engine_name.unwrap_or("None").to_string();
         let application_info = ApplicationInfoBuilder::<()>::new()
             .application_name(application_name)
             .engine_name(engine_name)
@@ -48,8 +51,9 @@ impl Instance {
         let instance_create_info = InstanceCreateInfoBuilder::<(), ()>::new()
             .enabled_extensions(match extensions {
                 Some(e) => Some(e),
-                None => None
+                None => None,
             })
+            .enabled_layer_names(layers)
             .application_info(application_info)
             .build();
         let create_info = instance_create_info.into_raw();
@@ -69,7 +73,8 @@ impl<'a> InstanceBuilder<'a> {
             vulkan_version: None,
             application_version: None,
             engine_version: None,
-            extensions: None
+            extensions: None,
+            layers: None,
         }
     }
     pub fn application_name(mut self, application_name: &'a str) -> Self {
@@ -93,7 +98,21 @@ impl<'a> InstanceBuilder<'a> {
         self
     }
     pub fn extensions(mut self, extensions: Vec<&'a str>) -> Self {
-        self.extensions = Some(extensions.into_iter().map(|x| x.to_string()).collect::<Vec<String>>());
+        self.extensions = Some(
+            extensions
+                .into_iter()
+                .map(|x| x.to_string())
+                .collect::<Vec<String>>(),
+        );
+        self
+    }
+    pub fn layers(mut self, layers: Vec<&'a str>) -> Self {
+        self.layers = Some(
+            layers
+                .into_iter()
+                .map(|x| x.to_string())
+                .collect::<Vec<String>>(),
+        );
         self
     }
     pub fn build(self) -> Arc<Instance> {
@@ -103,7 +122,8 @@ impl<'a> InstanceBuilder<'a> {
             self.vulkan_version,
             self.application_version,
             self.engine_version,
-            self.extensions
+            self.extensions,
+            self.layers,
         )
     }
 }
