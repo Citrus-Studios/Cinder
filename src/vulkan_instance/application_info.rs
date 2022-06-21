9ae198ff0a75d1f86cd4415c6f7c5adef26de79b
lonnonjamesd <lemonjameslem@protonmail.com>
2022-06-21 19:30:07 -0400

    Added fields for the struct

--- src/vulkan_instance/application_info.rs
+++ src/vulkan_instance/application_info.rs
@@ -1,5 +1,11 @@
 /// Safe Wrapper for Application Info to prevent unsafe headaches.
-pub struct SafeApplicationInfo {}
+pub struct SafeApplicationInfo {
+    application_name: String,
+    engine_name: String,
+    application_version: u32,
+    engine_version: u32,
+    api_version: u32,
+}
 
 impl SafeApplicationInfo {
     pub fn new() -> Self {
