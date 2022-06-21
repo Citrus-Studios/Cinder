5f6bc18d3d7be26eb504f0bae005cfd8b4c30ecc
lonnonjamesd <lemonjameslem@protonmail.com>
2022-06-21 19:29:49 -0400

    uncommented some code

--- examples/triangle/src/main.rs
+++ examples/triangle/src/main.rs
@@ -1,22 +1,23 @@
 use std::time::Duration;
 
-use sdl2::{sys::SDL_Vulkan_GetInstanceExtensions, keyboard::Keycode, event::Event};
+use sdl2::{event::Event, keyboard::Keycode, sys::SDL_Vulkan_GetInstanceExtensions};
 
 fn main() {
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
 
-    let window = video_subsystem.window("rust-sdl2 demo", 800, 600)
+    let window = video_subsystem
+        .window("rust-sdl2 demo", 800, 600)
         .position_centered()
         .vulkan()
         .build()
         .unwrap();
 
-    // let mut count:u32 = 0;
-    // unsafe { SDL_Vulkan_GetInstanceExtensions(window.raw(), &mut count, std::ptr::null_mut()) };
+    let mut count: u32 = 0;
+    unsafe { SDL_Vulkan_GetInstanceExtensions(window.raw(), &mut count, std::ptr::null_mut()) };
 
-    // let mut extensions = unsafe { zeroed_vec(count as usize) };
-    // unsafe { SDL_Vulkan_GetInstanceExtensions(window.raw(), &mut count, extensions.as_mut_ptr()) };
+    let mut extensions = unsafe { zeroed_vec(count as usize) };
+    unsafe { SDL_Vulkan_GetInstanceExtensions(window.raw(), &mut count, extensions.as_mut_ptr()) };
 
     let mut event_pump = sdl_context.event_pump().unwrap();
 
