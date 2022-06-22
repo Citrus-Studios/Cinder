5963339addfab6eb140500481addde67c757c1bb
lonnonjamesd <lemonjameslem@protonmail.com>
2022-06-21 22:43:33 -0400

    Fixed spelling error

--- src/functions.rs
+++ src/functions.rs
@@ -1,7 +1,7 @@
 /// Taken from https://www.khronos.org/registry/vulkan/specs/1.3-extensions/man/html/VK_MAKE_VERSION.html
 /// #define VK_MAKE_VERSION(major, minor, patch) \
 ///    ((((uint32_t)(major)) << 22) | (((uint32_t)(minor)) << 12) | ((uint32_t)(patch)
-#[deprecated(note = "Deprectaed in vulkan please use `make_api_version` instead")]
+#[deprecated(note = "Deprecated in vulkan please use `make_api_version` instead")]
 pub fn make_version(major: u8, minor: u8, patch: u8) -> u32 {
     (major as u32) << 22 | (minor as u32) << 12 | patch as u32
 }
