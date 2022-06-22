37498c818efba0109a39c9413f38b89dd0174868
lonnonjamesd <lemonjameslem@protonmail.com>
2022-06-21 23:14:45 -0400

    Updated the documentation to be better

--- src/vulkan_instance/application_info.rs
+++ src/vulkan_instance/application_info.rs
@@ -25,6 +25,8 @@ pub struct SafeApplicationInfo {
 impl SafeApplicationInfo {
     /// Creates a new SafeApplicationInfo from an application_name, engine_name, application_version, engine_version and api_version
     ///
+    /// Please use `make_api_version` from `cinder::functions` to create the `u32s` for the application_version, engine_version, and api_version arguments
+    ///
     /// ```rust
     /// use cinder::vulkan_instance::application_info::SafeApplicationInfo;
     /// use cinder::functions::make_api_version;
@@ -51,6 +53,11 @@ impl SafeApplicationInfo {
     }
     /// Creates a new SafeApplicationInfo from an application_name, engine_name, application_version, engine_version and api_version
     ///
+    /// Will panic if the application_name, engine_name and api_version are not in the correct format.
+    ///
+    /// Application and Engine version being in the format of `X.X.X`
+    ///
+    /// API version in the format of `X.X.X.X`
     /// ```rust
     /// use cinder::vulkan_instance::application_info::SafeApplicationInfo;
     /// use cinder::functions::make_api_version;
@@ -68,6 +75,7 @@ impl SafeApplicationInfo {
             .split(".")
             .map(|x| x.parse::<u8>().unwrap())
             .collect::<Vec<u8>>();
+        assert!(3 <= application_version.len());
         let application_version = make_api_version(
             0,
             application_version[0],
@@ -78,12 +86,14 @@ impl SafeApplicationInfo {
             .split(".")
             .map(|x| x.parse::<u8>().unwrap())
             .collect::<Vec<u8>>();
+        assert!(3 <= engine_version.len());
         let engine_version =
             make_api_version(0, engine_version[0], engine_version[1], engine_version[2]);
         let api_version = api_version
             .split(".")
             .map(|x| x.parse::<u8>().unwrap())
             .collect::<Vec<u8>>();
+        assert!(4 <= api_version.len());
         let api_version = make_api_version(
             api_version[0],
             api_version[1],
