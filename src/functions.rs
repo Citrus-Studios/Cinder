95af1b5033c89591b7a85b82f08209392a2c86cb
lonnonjamesd <lemonjameslem@protonmail.com>
2022-06-21 23:07:18 -0400

    Better Doc

--- src/functions.rs
+++ src/functions.rs
@@ -1,13 +1,11 @@
 /// Taken from https://www.khronos.org/registry/vulkan/specs/1.3-extensions/man/html/VK_MAKE_VERSION.html
-/// #define VK_MAKE_VERSION(major, minor, patch) \
-///    ((((uint32_t)(major)) << 22) | (((uint32_t)(minor)) << 12) | ((uint32_t)(patch)
+/// Creates a Vulkan version from 3 numbers
 #[deprecated(note = "Deprecated in vulkan please use `make_api_version` instead")]
 pub fn make_version(major: u8, minor: u8, patch: u8) -> u32 {
     (major as u32) << 22 | (minor as u32) << 12 | patch as u32
 }
 /// Taken from https://www.khronos.org/registry/vulkan/specs/1.3-extensions/man/html/VK_MAKE_API_VERSION.html
-/// #define VK_MAKE_API_VERSION(variant, major, minor, patch) \
-///     ((((uint32_t)(variant)) << 29) | (((uint32_t)(major)) << 22) | (((uint32_t)(minor)) << 12) | ((uint32_t)(patch)))
+/// Creates a Vulkan version from 4 numbers
 pub fn make_api_version(variant: u8, major: u8, minor: u8, patch: u8) -> u32 {
     (variant as u32) << 29 | (major as u32) << 22 | (minor as u32) << 12 | (patch as u32)
 }
