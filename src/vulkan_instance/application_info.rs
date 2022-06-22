eed4c281c3211017b4508ea1b4769d55fdc08311
lonnonjamesd <lemonjameslem@protonmail.com>
2022-06-21 22:52:57 -0400

    Created new_strings function

--- src/vulkan_instance/application_info.rs
+++ src/vulkan_instance/application_info.rs
@@ -1,3 +1,5 @@
+use crate::functions::make_api_version;
+
 /// Safe Wrapper for Application Info to prevent unsafe headaches.
 pub struct SafeApplicationInfo {
     pub application_name: String,
@@ -15,6 +17,47 @@ impl SafeApplicationInfo {
         engine_version: u32,
         api_version: u32,
     ) -> Self {
+        Self {
+            application_name: application_name.to_string(),
+            engine_name: engine_name.to_string(),
+            application_version,
+            engine_version,
+            api_version,
+        }
+    }
+    pub fn new_strings(
+        application_name: &str,
+        engine_name: &str,
+        application_version: &str,
+        engine_version: &str,
+        api_version: &str,
+    ) -> Self {
+        let application_version = application_version
+            .split(".")
+            .map(|x| x.parse::<u8>().unwrap())
+            .collect::<Vec<u8>>();
+        let application_version = make_api_version(
+            0,
+            application_version[0],
+            application_version[1],
+            application_version[2],
+        );
+        let engine_version = engine_version
+            .split(".")
+            .map(|x| x.parse::<u8>().unwrap())
+            .collect::<Vec<u8>>();
+        let engine_version =
+            make_api_version(0, engine_version[0], engine_version[1], engine_version[2]);
+        let api_version = api_version
+            .split(".")
+            .map(|x| x.parse::<u8>().unwrap())
+            .collect::<Vec<u8>>();
+        let api_version = make_api_version(
+            api_version[0],
+            api_version[1],
+            api_version[2],
+            api_version[3],
+        );
         Self {
             application_name: application_name.to_string(),
             engine_name: engine_name.to_string(),
