b81af9ef7ba2b423dbbe5a3f7d434ab7e91ede64
lonnonjamesd <lemonjameslem@protonmail.com>
2022-06-21 22:47:26 -0400

    Finished new function

--- src/vulkan_instance/application_info.rs
+++ src/vulkan_instance/application_info.rs
@@ -1,14 +1,26 @@
 /// Safe Wrapper for Application Info to prevent unsafe headaches.
 pub struct SafeApplicationInfo {
-    application_name: String,
-    engine_name: String,
-    application_version: u32,
-    engine_version: u32,
-    api_version: u32,
+    pub application_name: String,
+    pub engine_name: String,
+    pub application_version: u32,
+    pub engine_version: u32,
+    pub api_version: u32,
 }
 
 impl SafeApplicationInfo {
-    pub fn new() -> Self {
-        Self {}
+    pub fn new(
+        application_name: &str,
+        engine_name: &str,
+        application_version: u32,
+        engine_version: u32,
+        api_version: u32,
+    ) -> Self {
+        Self {
+            application_name: application_name.to_string(),
+            engine_name: engine_name.to_string(),
+            application_version,
+            engine_version,
+            api_version,
+        }
     }
 }
