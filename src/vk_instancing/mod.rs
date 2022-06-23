66afe8f808cd6458af25a306917a0de25aec0c32
lonnonjamesd <lemonjameslem@protonmail.com>
2022-06-23 15:24:17 -0400

    Added more pub use for shorter paths

--- src/vk_instancing/mod.rs
+++ src/vk_instancing/mod.rs
@@ -1,3 +1,7 @@
 pub mod vk_application_info;
 pub mod vk_create_info;
 pub mod vk_instance;
+
+pub use vk_application_info::SafeApplicationInfo;
+pub use vk_create_info::{CreateInfo, SafeCreateInfo};
+pub use vk_instance::SafeInstance;
