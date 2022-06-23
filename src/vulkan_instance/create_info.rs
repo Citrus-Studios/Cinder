006984d584cd2c8eac67774a54fc4053fcbbf1bb
lonnonjamesd <lemonjameslem@protonmail.com>
2022-06-23 15:12:21 -0400

    Created Unsafe CreateInfo Struct

--- src/vulkan_instance/create_info.rs
+++ src/vulkan_instance/create_info.rs
@@ -1,4 +1,4 @@
-use super::application_info::SafeApplicationInfo;
+use super::application_info::{self, SafeApplicationInfo};
 
 /// A Safe Wrapper for Create Info
 pub struct SafeCreateInfo<'a> {
@@ -10,6 +10,9 @@ pub struct SafeCreateInfo<'a> {
 }
 
 impl<'a> SafeCreateInfo<'a> {
+    pub fn new() -> Self {
+        todo!();
+    }
     /// Creates a new Safe Create Info
     ///
     /// ```rust
@@ -24,6 +27,32 @@ impl<'a> SafeCreateInfo<'a> {
         layer_names: Vec<String>,
         enabled_extension_count: u32,
         enabled_extension_names: Vec<String>,
+    ) -> Self {
+        Self {
+            application_info,
+            enabled_layer_count,
+            layer_names,
+            enabled_extension_count,
+            enabled_extension_names,
+        }
+    }
+}
+
+pub struct CreateInfo {
+    pub application_info: *const SafeApplicationInfo,
+    pub enabled_layer_count: u32,
+    pub layer_names: *const *const char,
+    pub enabled_extension_count: u32,
+    pub enabled_extension_names: *const *const char,
+}
+
+impl CreateInfo {
+    pub unsafe fn new(
+        application_info: *const SafeApplicationInfo,
+        enabled_layer_count: u32,
+        layer_names: *const *const char,
+        enabled_extension_count: u32,
+        enabled_extension_names: *const *const char,
     ) -> Self {
         Self {
             application_info,
