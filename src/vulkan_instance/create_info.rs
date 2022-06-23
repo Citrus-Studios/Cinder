f7f857838d7e091803aeb373f9f5115fb5f5f55f
lonnonjamesd <lemonjameslem@protonmail.com>
2022-06-23 13:46:47 -0400

    Changed function name

--- src/vulkan_instance/create_info.rs
+++ src/vulkan_instance/create_info.rs
@@ -3,6 +3,10 @@ use super::application_info::SafeApplicationInfo;
 /// A Safe Wrapper for Create Info
 pub struct SafeCreateInfo<'a> {
     pub application_info: &'a SafeApplicationInfo,
+    pub enabled_layer_count: u32,
+    pub layer_names: Vec<String>,
+    pub enabled_extension_count: u32,
+    pub enabled_extension_names: Vec<String>,
 }
 
 impl<'a> SafeCreateInfo<'a> {
@@ -12,9 +16,21 @@ impl<'a> SafeCreateInfo<'a> {
     /// use cinder::vulkan_instance::{create_info::SafeCreateInfo, application_info::SafeApplicationInfo};
     ///
     /// let application_info = SafeApplicationInfo::new_strings("Application Name", "Engine Name", "1.0.0", "1.0.0", "0.1.0.0");
-    /// let create_info = SafeCreateInfo::new(&application_info);
+    /// let create_info = SafeCreateInfo::new_from(&application_info);
     /// ```
-    pub fn new(application_info: &'a SafeApplicationInfo) -> Self {
-        Self { application_info }
+    pub fn new_from(
+        application_info: &'a SafeApplicationInfo,
+        enabled_layer_count: u32,
+        layer_names: Vec<String>,
+        enabled_extension_count: u32,
+        enabled_extension_names: Vec<String>,
+    ) -> Self {
+        Self {
+            application_info,
+            enabled_layer_count,
+            layer_names,
+            enabled_extension_count,
+            enabled_extension_names,
+        }
     }
 }
