a845af4a9ed2dfed112cb21d72d4e0f172438c0b
lonnonjamesd <lemonjameslem@protonmail.com>
2022-06-23 12:52:23 -0400

    Added Doc

--- src/vulkan_instance/create_info.rs
+++ src/vulkan_instance/create_info.rs
@@ -6,6 +6,14 @@ pub struct SafeCreateInfo<'a> {
 }
 
 impl<'a> SafeCreateInfo<'a> {
+    /// Creates a new Safe Create Info
+    ///
+    /// ```rust
+    /// use cinder::vulkan_instance::{create_info::SafeCreateInfo, application_info::SafeApplicationInfo};
+    ///
+    /// let application_info = SafeApplicationInfo::new_strings("Application Name", "Engine Name", "1.0.0", "1.0.0", "0.1.0.0");
+    /// let create_info = SafeCreateInfo::new(&application_info);
+    /// ```
     pub fn new(application_info: &'a SafeApplicationInfo) -> Self {
         Self { application_info }
     }
