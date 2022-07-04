2ebcb757e0550381faf8494af0651d188c78f741
lonnonjamesd <lemonjameslem@protonmail.com>
2022-07-04 05:22:50 -0400

    made the test actually work

--- tests/logging.rs
+++ tests/logging.rs
@@ -1,8 +1,11 @@
 use cinder::vk_instancing::vk_application_info::builder::ApplicationInfoBuilder;
-use tracing_test::traced_test;
+use tracing::info;
 
 #[tokio::test]
-#[traced_test]
-async fn logging_test() {
+async fn basic_logging_test() {
+    let subscriber = tracing_subscriber::FmtSubscriber::builder()
+        .with_max_level(tracing::Level::TRACE)
+        .finish();
+    tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
     let _ = ApplicationInfoBuilder::new().build();
 }
