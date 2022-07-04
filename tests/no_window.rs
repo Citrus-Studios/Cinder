2fbde06ac5d076ec8f8531b23fc12503d2635650
lonnonjamesd <lemonjameslem@protonmail.com>
2022-07-03 22:59:03 -0400

    created no_window example

---
+++ tests/no_window.rs
@@ -0,0 +1,11 @@
+use tracing::Level;
+use tracing_subscriber::FmtSubscriber;
+
+fn main() {
+    let subscriber = FmtSubscriber::builder()
+        // all spans/events with a level higher than TRACE (e.g, debug, info, warn, etc.)
+        // will be written to stdout.
+        .with_max_level(Level::TRACE)
+        // completes the builder.
+        .finish();
+}
