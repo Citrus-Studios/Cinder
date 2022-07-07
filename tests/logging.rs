9ccbf17eafa6383d1fa38e4c966b1c5aa44964a7
lonnonjamesd <lemonjameslem@protonmail.com>
2022-07-07 05:30:19 -0400

    updated logging example

--- tests/logging.rs
+++ tests/logging.rs
@@ -1,4 +1,7 @@
-use cinder::vk_instancing::vk_application_info::builder::ApplicationInfoBuilder;
+use cinder::vk_instancing::{
+    vk_application_info::builder::ApplicationInfoBuilder,
+    vk_create_info::builder::CreateInfoBuilder,
+};
 
 #[tokio::test]
 async fn basic_logging_test() {
@@ -6,5 +9,8 @@ async fn basic_logging_test() {
         .with_max_level(tracing::Level::TRACE)
         .finish();
     tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
-    let _ = ApplicationInfoBuilder::new().build();
+    let application_info = ApplicationInfoBuilder::new().build();
+    let _create_info = CreateInfoBuilder::new()
+        .with_application_info(&application_info)
+        .build();
 }
