a35695f4c455833a92d79833bbdabbb54470f940
lonnonjamesd <lemonjameslem@protonmail.com>
2022-07-11 11:36:11 -0400

    Updated Tests

--- tests/tests.rs
+++ tests/tests.rs
@@ -1,16 +1,35 @@
 use cinder::vk_instancing::{
-    vk_application_info::builder::ApplicationInfoBuilder,
+    vk_application_info::{builder::ApplicationInfoBuilder, r#unsafe::UnsafeApplicationInfo},
     vk_create_info::builder::CreateInfoBuilder,
+    UnsafeCreateInfo,
 };
+use mira::vulkan::{VkApplicationInfo, VkInstanceCreateInfo};
 
-#[tokio::test]
-async fn logging_test() {
+fn setup_logging() {
     let subscriber = tracing_subscriber::FmtSubscriber::builder()
         .with_max_level(tracing::Level::TRACE)
         .finish();
     tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
+}
+
+#[tokio::test]
+async fn logging_test() {
+    setup_logging();
     let application_info = ApplicationInfoBuilder::new().build();
     let _create_info = CreateInfoBuilder::new()
         .with_application_info(&application_info)
         .build();
+}
+
+#[tokio::test]
+async fn raw_conversion_test() {
+    setup_logging();
+    let application_info = ApplicationInfoBuilder::new().build();
+    let unsafe_application_info: UnsafeApplicationInfo = application_info.clone().into();
+    let _raw_application_info: VkApplicationInfo = unsafe_application_info.into();
+    let create_info = CreateInfoBuilder::new()
+        .with_application_info(&application_info)
+        .build();
+    let unsafe_create_info: UnsafeCreateInfo = create_info.clone().into();
+    let _raw_create_info: VkInstanceCreateInfo = unsafe_create_info.into();
 }
