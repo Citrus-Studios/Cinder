d60278a9b11bda64606b40f24d1f7b1ef8a581d1
lonnonjamesd <lemonjameslem@protonmail.com>
2022-07-12 07:07:46 -0400

    removed equality

--- src/vk_instancing/vk_create_info/safe.rs
+++ src/vk_instancing/vk_create_info/safe.rs
@@ -14,7 +14,7 @@ use crate::vk_instancing::SafeApplicationInfo;
 use super::builder::CreateInfoBuilder;
 
 /// A Safe Wrapper for Create Info
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone)]
 pub struct SafeCreateInfo<'a> {
     pub application_info: &'a SafeApplicationInfo,
     pub enabled_layer_count: u32,
