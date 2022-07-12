a3a814566d8eb616aa3e65d09607b66147b88640
lonnonjamesd <lemonjameslem@protonmail.com>
2022-07-12 02:56:40 -0400

    Added fields + new method

--- src/vk_instancing/vk_instance/safe.rs
+++ src/vk_instancing/vk_instance/safe.rs
@@ -1,4 +1,16 @@
-/// A Safe Wrapper for a Vulkan instance to try and prevent many, many headaches of unsafe
-pub struct SafeInstance {}
+use crate::vk_instancing::SafeCreateInfo;
 
-impl SafeInstance {}
+/// A Safe Wrapper for a Vulkan instance to try and prevent many, many headaches of unsafe
+pub struct SafeInstance<'a> {
+    create_info: &'a SafeCreateInfo<'a>,
+    allocator: Option<()>,
+}
+
+impl<'a> SafeInstance<'a> {
+    pub fn new(create_info: &'a SafeCreateInfo<'a>) -> Self {
+        Self {
+            create_info,
+            allocator: None,
+        }
+    }
+}
