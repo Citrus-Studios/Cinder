eeb25aa6c3a965d6aaec8ba8dd5e0be8fb5e53da
lonnonjamesd <lemonjameslem@protonmail.com>
2022-07-13 18:09:59 -0400

    implemented safe create_instance

--- src/functions.rs
+++ src/functions.rs
@@ -1,3 +1,14 @@
+use std::{
+    mem::zeroed,
+    ptr::{null, null_mut},
+};
+
+use mira::vulkan::VkInstance;
+
+#[allow(unused_imports)]
+use crate::unsafe_functions::*;
+use crate::vk_instancing::CreateInfo;
+
 /// # Taken from https://www.khronos.org/registry/vulkan/specs/1.3-extensions/man/html/VK_MAKE_VERSION.html<br/>
 /// Creates a Vulkan version from 3 numbers
 #[deprecated(note = "Deprecated in vulkan please use `make_api_version` instead")]
@@ -8,4 +19,29 @@ pub fn make_version(major: u8, minor: u8, patch: u8) -> u32 {
 /// Creates a Vulkan version from 4 numbers
 pub fn make_api_version(variant: u8, major: u8, minor: u8, patch: u8) -> u32 {
     (variant as u32) << 29 | (major as u32) << 22 | (minor as u32) << 12 | (patch as u32)
+}
+
+pub(crate) fn create_instance(
+    create_info: Option<CreateInfo>,
+    allocator: Option<()>,
+) -> Result<VkInstance, String> {
+    unsafe {
+        let mut instance: VkInstance = zeroed();
+        let result = vkCreateInstance(
+            match create_info {
+                Some(create_info) => &(create_info.into()),
+                None => null(),
+            },
+            match allocator {
+                Some(allocator) => null_mut(),
+                None => null_mut(),
+            },
+            &mut instance,
+            None,
+        );
+        match result {
+            VK_SUCCESS => Ok(instance),
+            _ => Err(result.match_vulkan_error_code().to_string()),
+        }
+    }
 }
