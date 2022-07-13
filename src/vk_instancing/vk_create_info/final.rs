b1c409d808c0934b859bf0dbad06c041bfcd882b
lonnonjamesd <lemonjameslem@protonmail.com>
2022-07-13 12:23:21 -0400

    Implemented the struct

---
+++ src/vk_instancing/vk_create_info/final.rs
@@ -0,0 +1,20 @@
+use mira::vulkan::VkInstanceCreateInfo;
+
+use crate::vk_instancing::{SafeCreateInfo, UnsafeCreateInfo};
+
+pub struct CreateInfo<'a> {
+    pub raw: VkInstanceCreateInfo,
+    pub normal: SafeCreateInfo<'a>,
+}
+
+impl<'a> CreateInfo<'a> {
+    pub fn new(create_info: SafeCreateInfo<'a>) -> Self {
+        Self {
+            raw: <SafeCreateInfo as Into<UnsafeCreateInfo>>::into(create_info.clone()).into(),
+            normal: create_info,
+        }
+    }
+    pub fn rebuild(&mut self) {
+        self.raw = <SafeCreateInfo as Into<UnsafeCreateInfo>>::into(self.normal.clone()).into();
+    }
+}
