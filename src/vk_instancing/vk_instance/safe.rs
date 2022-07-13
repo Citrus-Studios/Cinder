9098a13fe2ebc253aaa538c34862fd65b0be719f
lonnonjamesd <lemonjameslem@protonmail.com>
2022-07-13 12:43:32 -0400

    removed reference

--- src/vk_instancing/vk_instance/safe.rs
+++ src/vk_instancing/vk_instance/safe.rs
@@ -2,12 +2,12 @@ use crate::vk_instancing::CreateInfo;
 
 /// A Safe Wrapper for a Vulkan instance to try and prevent many, many headaches of unsafe
 pub struct SafeInstance<'a> {
-    create_info: &'a CreateInfo<'a>,
+    create_info: CreateInfo<'a>,
     allocator: Option<()>,
 }
 
 impl<'a> SafeInstance<'a> {
-    pub fn new(create_info: &'a CreateInfo<'a>) -> Self {
+    pub fn new(create_info: CreateInfo<'a>) -> Self {
         Self {
             create_info,
             allocator: None,
