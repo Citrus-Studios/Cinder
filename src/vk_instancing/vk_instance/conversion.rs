212c0a62df16398e036888668f5a657e23af6016
lonnonjamesd <lemonjameslem@protonmail.com>
2022-07-13 18:14:18 -0400

    Added debug message

--- src/vk_instancing/vk_instance/conversion.rs
+++ src/vk_instancing/vk_instance/conversion.rs
@@ -1,9 +1,14 @@
+use mira::vulkan::VkInstance;
+use tracing::debug;
+
 use crate::vk_instancing::SafeInstance;
 
 use super::r#unsafe::UnsafeInstance;
 
 impl<'a> Into<UnsafeInstance<'a>> for SafeInstance<'a> {
     fn into(self) -> UnsafeInstance<'a> {
+        #[cfg(feature = "detailed-logging")]
+        debug!("Converting `SafeInstance` into `UnsafeInstance`");
         UnsafeInstance {
             create_info: self.create_info,
             allocator: std::ptr::null(),
