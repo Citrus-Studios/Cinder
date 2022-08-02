9e15d82af6db2fffb5b865293bb4028fa757aebc
lonnonjamesd <lemonjameslem@protonmail.com>
2022-08-01 21:31:40 -0400

    Added the structs to the pub mod

--- src/vk_instancing/mod.rs
+++ src/vk_instancing/mod.rs
@@ -6,7 +6,7 @@ pub use vk_application_info::{
     r#final::ApplicationInfo, r#unsafe::UnsafeApplicationInfo, safe::SafeApplicationInfo,
 };
 pub use vk_create_info::{r#final::CreateInfo, r#unsafe::UnsafeCreateInfo, safe::SafeCreateInfo};
-pub use vk_instance::safe::SafeInstance;
+pub use vk_instance::{r#final::Instance, r#unsafe::UnsafeInstance, safe::SafeInstance};
 
 #[cfg(feature = "builder")]
 pub use vk_application_info::builder::ApplicationInfoBuilder;
